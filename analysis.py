9e8894fb08ba17ff07268c939be7c4dd534c5bf5
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-07-03 09:41:09 +0300

    Finished Stage 4's code and docstring

--- analysis.py
+++ analysis.py
@@ -8,6 +8,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import Normalizer
 
 """Stage 1: The Keras dataset
 
@@ -125,4 +126,56 @@ max_score = max(scores)
 index_max_score = scores.index(max_score)
 
 print(f'The answer to the question: {models[index_max_score].__name__} - \
-{max_score}')
+{max_score}\n')
+
+
+"""Stage 4: Data preprocessing
+
+Description
+
+At this stage, we will improve model performance by preprocessing the data. We 
+will see how normalization affects the accuracy.
+
+Objectives
+
+1 - Import sklearn.preprocessing.Normalizer transformer
+2 - Initialize the normalizer and transform the features (x_train and x_test)
+3 - Re-run the function <fit_predict_eval> but on the normalized data
+4 - Answer the questions:
+    - Does the normalization have a positive impact in general?
+    - Which two models show the best scores?
+
+"""
+
+# initialize the normalizer and transform the features x_train and x_test
+train_transformer = Normalizer().fit(x_train)
+x_train_norm = train_transformer.transform(x_train)
+
+test_transformer = Normalizer().fit(x_test)
+x_test_norm = test_transformer.transform(x_test)
+
+print("Re-running the models but on the normalized data instead:\n")
+# Re-run the function on each of the four models but with the normalized data
+normalized_scores = []
+for model in models:
+    model_score = fit_predict_eval(model(), x_train_norm, x_test_norm, y_train,
+                                 y_test)
+    normalized_scores.append(model_score)
+
+print(f'The answer to the 1st question: yes\n')
+
+# To help find the best scores
+temp_copy = normalized_scores[:]
+temp_copy.sort(reverse=True)
+
+# The model that showed the best score
+max_normalized_score = temp_copy[0]
+index_max_normalized_score = normalized_scores.index(max_normalized_score)
+
+# The model that showed the second best score
+second_normalized_scored = temp_copy[1]
+index_second_normalized_score = normalized_scores.index(second_normalized_scored)
+
+print(f'The answer to the 2nd question: \
+{models[index_max_normalized_score].__name__}-{max_normalized_score}, \
+{models[index_second_normalized_score].__name__}-{second_normalized_scored}')
